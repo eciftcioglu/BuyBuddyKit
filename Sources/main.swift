3b0879de5c60ca45f2afc96d1b659fef3e88a4a1
Buğra Ekuklu <ekuklu@icloud.com>
2017-02-25 19:44:41 +0300

    Unit testing fixture.

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,13 +9,17 @@
 import Foundation
 import Alamofire
 
-Alamofire.request("https://httpbin.org/get").responseJSON { response in
-    print(response.request)  // original URL request
-    print(response.response) // HTTP URL response
-    print(response.data)     // server data
-    print(response.result)   // result of response serialization
-    
-    if let JSON = response.result.value {
-        print("JSON: \(JSON)")
-    }
+//Alamofire.request("https://httpbin.org/get").responseJSON { response in
+//    print(response.request)  // original URL request
+//    print(response.response) // HTTP URL response
+//    print(response.data)     // server data
+//    print(response.result)   // result of response serialization
+//    
+//    if let JSON = response.result.value {
+//        print("JSON: \(JSON)")
+//    }
+//}
+
+public func fn() -> Bool {
+    return true
 }
--- Tests/SimpleTest.swift
+++ Tests/SimpleTest.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import BuyBuddyKit
 import XCTest
 
 class SimpleTest: XCTestCase {
@@ -23,13 +24,7 @@ class SimpleTest: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert(BuyBuddyKit.fn(), true)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
