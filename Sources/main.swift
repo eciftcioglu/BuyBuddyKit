03bada1ea4ce5a71005cfbced391380730c4e047
Buğra Ekuklu <ekuklu@icloud.com>
2017-02-25 19:30:38 +0300

    Added unit testing infrastructure

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,5 +7,15 @@
 //
 
 import Foundation
+import Alamofire
 
-print("Hello, world")
+Alamofire.request("https://httpbin.org/get").responseJSON { response in
+    print(response.request)  // original URL request
+    print(response.response) // HTTP URL response
+    print(response.data)     // server data
+    print(response.result)   // result of response serialization
+    
+    if let JSON = response.result.value {
+        print("JSON: \(JSON)")
+    }
+}
