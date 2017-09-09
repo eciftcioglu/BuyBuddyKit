66222eb45342da14f2415254c0e812f8d3d31157
Buğra Ekuklu <ekuklu@icloud.com>
2017-09-09 10:30:00 +0300

    Initial Commit

---
+++ BuyBuddyKit/BuyBuddyKit.h
@@ -0,0 +1,19 @@
+//
+//  BuyBuddyKit.h
+//  BuyBuddyKit
+//
+//  Created by Buğra Ekuklu on 9.09.2017.
+//  Copyright © 2017 BuyBuddy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BuyBuddyKit.
+FOUNDATION_EXPORT double BuyBuddyKitVersionNumber;
+
+//! Project version string for BuyBuddyKit.
+FOUNDATION_EXPORT const unsigned char BuyBuddyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuyBuddyKit/PublicHeader.h>
+
+
---
+++ BuyBuddyKitTests/BuyBuddyKitTests.m
@@ -0,0 +1,39 @@
+//
+//  BuyBuddyKitTests.m
+//  BuyBuddyKitTests
+//
+//  Created by Buğra Ekuklu on 9.09.2017.
+//  Copyright © 2017 BuyBuddy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BuyBuddyKitTests : XCTestCase
+
+@end
+
+@implementation BuyBuddyKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
