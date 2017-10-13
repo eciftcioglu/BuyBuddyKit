cbd8de64dec0cb9d5c6a654a08e6aef6380c9e85
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-13 04:50:00 +0300

    Removed dangling import from tests.

--- Tests/BuyBuddyKit Tests/BBKConnectionContextTests.m
+++ Tests/BuyBuddyKit Tests/BBKConnectionContextTests.m
@@ -21,14 +21,11 @@
 // THE SOFTWARE.
 
 #import "BBKTestCase.h"
-#import "BBKConnectionContextFactory.h"
 #import "BBKConnectionContext.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BBKConnectionContextTests : BBKTestCase
-
-@property BBKConnec
 
 @end
 
